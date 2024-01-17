a2251e04d222882100e5c2aa85a9c3b6500b4969
Nathan Geronimo <nathangero@yahoo.com>
2024-01-17 13:31:56 -0800

    add firebase package

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from 'react'
 import './App.css';
 import Nav from "./components/Navbar";
 import { Outlet } from "react-router";
+import { useEffect } from 'react';
+import { auth } from "../firebase.js";
 
 function App() {
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      // console.log("@onAuthStateChanged");
+      if (user?.uid) {
+        console.log("logging in user:", user.uid);
+      }
+    });
+  }, []);
 
   return (
     <>
