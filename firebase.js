5b8e1c792f78063c0576a5e7fe4bcc80e08aab93
Nathan Geronimo <nathangero@yahoo.com>
2024-01-17 14:09:37 -0800

    try using process

--- firebase.js
+++ firebase.js
@@ -3,23 +3,25 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
+import { config } from "dotenv";
+config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_PROJECT_KEY,
-  authDomain: import.meta.env.VITE_PROJECT_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_PROJECT_BUCKET,
-  messagingSenderId: import.meta.env.VITE_PROJECT_MESSAGING,
-  appId: import.meta.env.VITE_PROJECT_APP_ID,
-  measurementId: import.meta.env.VITE_PROJECT_MEASUREMENT,
+  apiKey: process.env.VITE_PROJECT_KEY,
+  authDomain: process.env.VITE_PROJECT_DOMAIN,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_PROJECT_BUCKET,
+  messagingSenderId: process.env.VITE_PROJECT_MESSAGING,
+  appId: process.env.VITE_PROJECT_APP_ID,
+  measurementId: process.env.VITE_PROJECT_MEASUREMENT,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-export { app, auth, db };
+export { app, analytics, auth, db };
