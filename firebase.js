04c077a4ec41d20ec67c9c7458c6ac22d97e7db3
Nathan Geronimo <nathangero@yahoo.com>
2024-01-17 12:42:32 -0800

    add firebase

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_PROJECT_KEY,
+  authDomain: import.meta.env.VITE_PROJECT_ID,
+  projectId: import.meta.env.VITE_PROJECT_DOMAIN,
+  storageBucket: import.meta.env.VITE_PROJECT_BUCKET,
+  messagingSenderId: import.meta.env.VITE_PROJECT_MESSAGING,
+  appId: import.meta.env.VITE_PROJECT_APP_ID,
+  measurementId: import.meta.env.VITE_PROJECT_MEASUREMENT,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
