7f5aaed285f06d3308c23175b2a4fdce34f43687
Nathan Geronimo <nathangero@yahoo.com>
2024-01-18 15:23:23 -0800

    add basic template with editing fields

--- src/components/Template/index.jsx
+++ src/components/Template/index.jsx
@@ -6,15 +6,16 @@ export default function Template() {
   const [companyName, setCompanyName] = useState("COMPANY_NAME");
   const [companyHiringManager, setCompanyHiringManager] = useState("HIRING_MANAGER");
   const [companyValues, setCompanyValues] = useState([]);
+  const [value, setValue] = useState("");
   const [useCompanyName, showCompanyName] = useState(true); // Show either company name or hiring manager
 
   const [userFullName, setUserFullName] = useState("YOUR_FULL_NAME");
   const [userEmail, setUserEmail] = useState("YOUR_EMAIL");
   const [userPhone, setUserPhone] = useState("YOUR_PHONE");
   const [userOccupation, setOccupation] = useState("YOUR_OCCUPATION");
-  const [userOccupationLocation, setOccupationLocation] = useState("YOUR_OCCUPATION_LOCATION");
+  const [userOccupationWorkplace, setOccupationWorkplace] = useState("YOUR_WORKPLACE");
   const [userStartDate, setStartDate] = useState("START_DATE");
-  const [userAboutMe, setAboutMe] = useState("YOUR_RELAVENT_WORK_EXPERIENCE_EXTRACURRICULARS_PROJECTS");
+  const [userAboutMe, setAboutMe] = useState("YOUR_EXPERIENCE_PROJECTS");
 
   const useAorAn = () => {
     const firstLetter = userOccupation[0];
@@ -34,11 +35,15 @@ export default function Template() {
       return companyValues[0];
     }
 
+    console.log("companyValues:", companyValues);
+
     let valueStr;
     companyValues.map((value, index) => {
-      if (index === companyValues.length - 1) {
+      console.log("index:", index, "value:", value);
+      if (!value) return;
+      if (index === companyValues.length - 1 && companyValues.length > 1) {
         valueStr += `and ${value}`
-      } else {
+      } else if (companyValues.length > 2) {
         // Determine if a comma should go before "and" is appended
         valueStr += (index === companyValues.length - 2) ? value : `${value}, `;
       }
@@ -47,13 +52,63 @@ export default function Template() {
     return valueStr;
   }
 
+  const onChangeCompanyName = ({ target }) => {
+    setCompanyName(target.value);
+  }
+
+  const onChangeHiringManager = ({ target }) => {
+    setCompanyHiringManager(target.value);
+  }
+
+  const onChangeCompanyValue = ({ target }) => {
+    setValue(target.value);
+  }
+
+  const onChangeFullName = ({ target }) => {
+    setUserFullName(target.value);
+  }
+
+  const onChangeOccupation = ({ target }) => {
+    setOccupation(target.value);
+  }
+
+  const onChangeOccupationWorkplace = ({ target }) => {
+    setOccupationWorkplace(target.value);
+  }
+
+  const onChangeEmail = ({ target }) => {
+    setUserEmail(target.value);
+  }
+
+  const onChangePhone = ({ target }) => {
+    setUserPhone(target.value);
+  }
+
+  const onChangeStartDate = ({ target }) => {
+    setStartDate(target.value);
+  }
+
+  const onChangeAboutMe = ({ target }) => {
+    setAboutMe(target.value);
+  }
+
+  const onSubmitValue = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (!value) { alert("Can't add a blank value"); return; }
+
+    setCompanyValues([...companyValues, value]);
+    setValue("");
+  }
+
   return (
     <>
       <div>
         <h1>Template</h1>
         <div className="fs-5 template">
           <p>Dear <span className="user-input">{useCompanyName ? companyName : companyHiringManager}</span>,</p>
-          <p>I hope your day is going well! My name is <span className="user-input">{userFullName}</span>, and I&apos;m {useAorAn()} at <span className="user-input">{userOccupationLocation}</span>. I am very interested in working for <span className="user-input">{companyName}</span> and can start <span className="user-input">{userStartDate}</span>. Your commitment to <span className="user-input">{listCompanyValues()}</span> that I saw on the website inspired me! The products you build and the values you stand for make <span className="user-input">{companyName}</span> seem like an ideal workplace for me.</p>
+          <p>I hope your day is going well! My name is <span className="user-input">{userFullName}</span>, and I&apos;m {useAorAn()} <span className="user-input">{userOccupation}</span> at <span className="user-input">{userOccupationWorkplace}</span>. I am very interested in working for <span className="user-input">{companyName}</span> and can start <span className="user-input">{userStartDate}</span>. Your commitment to <span className="user-input">{listCompanyValues()}</span> that I saw on the website inspired me! The products you build and the values you stand for make <span className="user-input">{companyName}</span> seem like an ideal workplace for me.</p>
           <p>A little about me, <span className="user-input">{userAboutMe}</span></p>
           <p>I think these experiences would make me a great candidate for you.</p>
           <p>Please let me know if there&apos;s anything else you need from me. I look forward to hearing from you! I can be reached at <span className="user-input">{userEmail}</span> and <span className="user-input">{userPhone}</span>.</p>
@@ -61,9 +116,62 @@ export default function Template() {
         </div>
       </div>
 
-      <div>
+      <div className="container-fluid row">
         <h1>Your Edits</h1>
+        <div className="input-row">
+          <label className="mx-3">Company Name:</label>
+          <input className="form-control" onChange={onChangeCompanyName} placeholder="Cool Place Inc." />
+        </div>
+        <div className="input-row">
+          <label className="mx-3">Hiring Manager:</label>
+          <input className="form-control" onChange={onChangeHiringManager} placeholder="Their Name" />
+        </div>
+        <div className="input-row">
+          <label className="mx-3">Your Name:</label>
+          <input className="form-control" onChange={onChangeFullName} placeholder="First Last" />
+        </div>
+        <div className="input-row">
+          <label className="mx-3">Your Occupation:</label>
+          <input className="form-control" onChange={onChangeOccupation} placeholder="Cool Job Title" />
+        </div>
+        <div className="input-row">
+          <label className="mx-3">Your Workplace:</label>
+          <input className="form-control" onChange={onChangeOccupationWorkplace} placeholder="e.g. Cool Job Inc." />
+        </div>
+        <div className="input-row">
+          <label className="mx-3">Your Email:</label>
+          <input className="form-control" type="email" onChange={onChangeEmail} placeholder="test@example.com" />
+        </div>
+        <div className="input-row">
+          <label className="mx-3">Your Phone #:</label>
+          <input className="form-control" type="tel" onChange={onChangePhone} placeholder="e.g 555-555-5555" />
+        </div>
+        <div className="input-row">
+          <label className="mx-3">Start Date:</label>
+          <input className="form-control" onChange={onChangeStartDate} placeholder="e.g. 01/23/24" />
+        </div>
 
+
+        <div className="input-row">
+          <form onSubmit={onSubmitValue}>
+            <label className="mx-3">Company Values:</label>
+            <input className="form-control me-3" value={value} onChange={onChangeCompanyValue} placeholder="One value at a time" />
+            <button type="submit" className="btn btn-primary">Add Value</button>
+          </form>
+          {companyValues?.map((value, index) => (
+            <>
+              <div key={index} className="company-value">
+                <p>{value}</p>
+                <button className="btn"><i className="bi bi-x-lg"></i></button>
+              </div>
+            </>
+          ))}
+        </div>
+
+        <div className="input-row">
+          <label className="mx-3">About Me:</label>
+          <textarea className="form-control" onChange={onChangeAboutMe} placeholder="List your relevant work experience, extracurriculars, and projects" />
+        </div>
       </div>
     </>
   )
